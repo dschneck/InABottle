24d314a15a334a204a33dcafd60180c167385509
David Schneck <david.schneckster@hotmail.com>
2020-10-19 19:34:00 -0400

    Trying to connect to MongoDb server

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
-const {  ApolloServer } = require('apollo-server');
-
+const {  ApolloServer } = require('apollo-server'); // { x } destructures x
 const gql = require('graphql-tag'); // dependency of appollo-server
+const mongoose = require('mongoose');
+const { MONGODB } = require('./config.js');
 
-// "!" means that its required
 const typeDefs = gql` 
+    # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+    # "!" means that its required
+
     type Query {
         sayHi: String!  
     }
-`
+`;
 const resolvers = {
     Query: {
         sayHi: () => 'Hello World!'
     }
 }
 
-const server = new ApolloServer({
-    typeDefs,
-    resolvers
-});
+const server = new ApolloServer({typeDefs, resolvers});
 
-server.listen({port: 5000})
-    .then(res => {
-        console.log(`Server running at ${res.url}`);
+mongoose.connect(MONGODB, { useNewUrlParser: true })
+    .then(() => {
+        console.log('MongoDb connected');
+        return server.listen({port: 5000});
     })
+    .then((res) => {
+        console.log(`Server is running at ${res.url}`);
+    });
+    

5a38854d9c734cefed432630279a6beaf9bf3b7a
David Schneck <david.schneckster@hotmail.com>
2020-10-19 22:38:07 -0400

    I've successfuly connected my MongoDB database and created 2 models for my data. Those being Posts and Users, however, I will certainly be changing them in the future.

--- index.js
+++ index.js
@@ -1,25 +1,44 @@
+// Dependancy Imports
 const {  ApolloServer } = require('apollo-server'); // { x } destructures x
 const gql = require('graphql-tag'); // dependency of appollo-server
 const mongoose = require('mongoose');
-const { MONGODB } = require('./config.js');
+
+// Relative imports
+const config = require('./config.js');
+const Post = require('./models/Post.js');
+//const Post = require('./models/Post.js');
 
 const typeDefs = gql` 
     # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
     # "!" means that its required
 
+    type Post {
+        id: ID!
+        body: String!
+        createdAt: String! # Its going to take a little extra work to make this date
+        username: String!
+    }
     type Query {
-        sayHi: String!  
+        getPosts: [Post]  
     }
 `;
 const resolvers = {
     Query: {
-        sayHi: () => 'Hello World!'
+        async getPosts() {
+            try{
+                const posts = await Post.find();
+                return posts;
+            }catch(error){
+                throw new Error(err);
+            }
+            
+        }
     }
-}
+};
 
 const server = new ApolloServer({typeDefs, resolvers});
 
-mongoose.connect(MONGODB, { useNewUrlParser: true })
+mongoose.connect(config.db.connection, {useNewUrlParser:true , useUnifiedTopology: true})
     .then(() => {
         console.log('MongoDb connected');
         return server.listen({port: 5000});
